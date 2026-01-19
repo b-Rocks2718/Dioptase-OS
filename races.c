42806a3bf8f29453da7ad15b47a9d70ee0f60c57
Brooks Bryant <brooks.bryant@icloud.com>
2026-01-19 03:52:36 -0600

    oops'

--- races.c
+++ races.c
@@ -1,8 +1,6 @@
 #include "kernel/machine.h"
 #include "kernel/print.h"
 #include "kernel/atomic.h"
-#include "kernel/config.h"
-
 
 int spinlock = 0;
 
@@ -10,35 +8,25 @@ int spinlock_counter = 0;
 int atomic_counter = 0;
 int unsafe_counter = 0;
 
-int spinlock_test(void){
-  // test spinlocks by having all cores increment a shared counter
+int kernel_entry(void) {
+  // tests have multiple cores increment a shared counter
+
+  // test spinlocks
   for (int i = 0; i < 1000; ++i) {
     get_spinlock(&spinlock);
     spinlock_counter++;
     release_spinlock(&spinlock);
   }
-}
 
-int atomic_test(void){
-  // test atomics by having all cores increment a shared counter
+  // test atomics
   for (int i = 0; i < 1000; ++i) {
     __atomic_fetch_add(&atomic_counter, 1);
   }
-}
 
-int unsafe_test(void){
-  // test unsafe increments by having all cores increment a shared counter
+  // test unsafe increments
   for (int i = 0; i < 1000; ++i) {
     unsafe_counter++;
   }
-}
-
-int kernel_entry(void) {
-  spinlock_test();
-
-  atomic_test();
-
-  unsafe_test();
 
   if (get_core_id() == 0) {
     puts("spinlock_counter: ");
