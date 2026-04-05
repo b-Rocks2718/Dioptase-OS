b7ee8ee320bba3379de87611b23860f18db58cff
Brooks Bryant <brooks.bryant@icloud.com>
2026-04-04 22:24:34 -0500

    new high score!

--- kernel/physmem.h
+++ kernel/physmem.h
@@ -2,8 +2,10 @@
 #define PHYSMEM_H
 
 #define FRAME_SIZE 4096
-#define FRAMES_ADDR_START 4096
+#define FRAMES_ADDR_START 0x800000
 #define FRAMES_ADDR_END 0x7FBD000
+
+#define PHYS_FRAME_COUNT 30653
 
 // free pages store metadata to form a linked list
 struct FreePageNode {
